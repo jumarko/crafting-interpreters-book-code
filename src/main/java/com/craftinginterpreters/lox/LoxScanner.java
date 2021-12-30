ea7f4b3fac76fd2265452bbcd98cac9764d20e7c
Juraj Martinka <jumarko@gmail.com>
2021-12-30 22:52:53 +0100

    Basic implementation of LoxScanner

--- src/main/java/com/craftinginterpreters/lox/LoxScanner.java
+++ src/main/java/com/craftinginterpreters/lox/LoxScanner.java
@@ -1,16 +1,65 @@
 package com.craftinginterpreters.lox;
 
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class LoxScanner {
-    public LoxScanner(String source) {
+/**
+ * See https://craftinginterpreters.com/scanning.html#the-scanner-class
+ */
+class LoxScanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
 
+    // these fields are mutable!
+    private int start = 0; // first char of the lexeme being scanned
+    private int current = 0; // current char of the current lexeme
+    private int line = 1; // line on which the current char is
+
+    LoxScanner(String source) {
+        this.source = source;
     }
 
-    public List<Token> scanTokens() {
-        // TODO: parse tokens
-        return Collections.emptyList();
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return Collections.unmodifiableList(tokens);
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(' -> addToken(TokenType.LEFT_PAREN);
+            case ')' -> addToken(TokenType.RIGHT_PAREN);
+            case '{' -> addToken(TokenType.LEFT_BRACE);
+            case '}' -> addToken(TokenType.RIGHT_BRACE);
+            case ',' -> addToken(TokenType.COMMA);
+            case '.' -> addToken(TokenType.DOT);
+            case '-' -> addToken(TokenType.MINUS);
+            case '+' -> addToken(TokenType.PLUS);
+            case ';' -> addToken(TokenType.SEMICOLON);
+            case '*' -> addToken(TokenType.STAR);
+        }
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
     }
 }
--- src/main/java/com/craftinginterpreters/lox/Token.java
+++ src/main/java/com/craftinginterpreters/lox/Token.java
@@ -2,14 +2,14 @@ package com.craftinginterpreters.lox;
 
 public class Token {
 
-    private final String lexeme;
     private final TokenType type;
+    private final String lexeme;
     private final Object literal;
     private final int line;
 
-    Token(String lexeme, TokenType type, Object literal, int line) {
-        this.lexeme = lexeme;
+    Token(TokenType type, String lexeme, Object literal, int line) {
         this.type = type;
+        this.lexeme = lexeme;
         this.literal = literal;
         this.line = line;
     }
